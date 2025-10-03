0605b73dc2539b44d87df2c7a205e812fec34edb
Jason Walker <jason@onetrue.name>
2025-10-03 08:48:26 -0700

    Updated power settings

--- modules/home/base/plasma.nix
+++ modules/home/base/plasma.nix
@@ -9,68 +9,83 @@ _: {
     };
 
     powerdevil = {
+      general = {
+        pausePlayersOnSuspend = true;
+      };
+
       AC = {
-        powerButtonAction = "lockScreen";
-        autoSuspend = {
-          action = "nothing";
-          idleTimeout = null;
-        };
-        turnOffDisplay = {
-          idleTimeout = 60 * 15;
-          idleTimeoutWhenLocked = 60 * 5;
-        };
+        powerProfile = "performance";
+        whenSleepingEnter = "standby";
+        displayBrightness = null;
+
+        powerButtonAction = "sleep";
+        whenLaptopLidClosed = "sleep";
+        inhibitLidActionWhenExternalMonitorConnected = true;
+
         dimDisplay = {
           enable = true;
           idleTimeout = 60 * 5;
         };
-        whenLaptopLidClosed = "sleep";
-        inhibitLidActionWhenExternalMonitorConnected = true;
-        powerProfile = "balanced";
-        displayBrightness = null;
-      };
 
-      battery = {
-        powerButtonAction = "lockScreen";
+        turnOffDisplay = {
+          idleTimeout = 60 * 10;
+          idleTimeoutWhenLocked = 60 * 5;
+        };
+
         autoSuspend = {
           action = "sleep";
           idleTimeout = 60 * 15;
         };
-        turnOffDisplay = {
-          idleTimeout = 60 * 5;
-          idleTimeoutWhenLocked = 60;
-        };
+      };
+
+      battery = {
+        powerProfile = "balanced";
+        whenSleepingEnter = "hybridSleep";
+        displayBrightness = 50;
+
+        powerButtonAction = "sleep";
+        whenLaptopLidClosed = "sleep";
+        inhibitLidActionWhenExternalMonitorConnected = false;
+
         dimDisplay = {
           enable = true;
           idleTimeout = 60 * 2;
         };
-        whenLaptopLidClosed = "sleep";
-        inhibitLidActionWhenExternalMonitorConnected = false;
-        powerProfile = "powerSaving";
-        displayBrightness = 50;
+
+        turnOffDisplay = {
+          idleTimeout = 60 * 5;
+          idleTimeoutWhenLocked = 60;
+        };
+
+        autoSuspend = {
+          action = "sleep";
+          idleTimeout = 60 * 10;
+        };
       };
 
       lowBattery = {
-        powerButtonAction = "lockScreen";
-        autoSuspend = {
-          action = "hibernate";
-          idleTimeout = 60 * 5;
-        };
-        turnOffDisplay = {
-          idleTimeout = 60;
-          idleTimeoutWhenLocked = 30;
-        };
+        powerProfile = "powerSaving";
+        whenSleepingEnter = "standbyThenHibernate";
+        displayBrightness = 30;
+
+        powerButtonAction = "hibernate";
+        whenLaptopLidClosed = "hibernate";
+        inhibitLidActionWhenExternalMonitorConnected = false;
+
         dimDisplay = {
           enable = true;
           idleTimeout = 60;
         };
-        whenLaptopLidClosed = "hibernate";
-        inhibitLidActionWhenExternalMonitorConnected = false;
-        powerProfile = "powerSaving";
-        displayBrightness = 30;
-      };
 
-      general = {
-        pausePlayersOnSuspend = true;
+        turnOffDisplay = {
+          idleTimeout = 60 * 2;
+          idleTimeoutWhenLocked = 60;
+        };
+
+        autoSuspend = {
+          action = "hibernate";
+          idleTimeout = 60 * 5;
+        };
       };
     };
   };
