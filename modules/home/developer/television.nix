3a080d14a025f76d86c1eb9e38b316b538933ce9
Jason Walker <jason@onetrue.name>
2026-02-11 21:28:36 -0800

    Add upstream television cable channels via builtins.fromTOML
    Read channel definitions directly from the television package source
    (pkgs.television.src/cable/unix/) instead of duplicating TOML content.
    
    Channels are selected based on the tools in this config:
    - Git (branch, diff, log, reflog, remotes, repos, stash, submodules, tags)
    - GitHub CLI (issues, prs)
    - Docker/Podman (containers, images, compose, networks, volumes)
    - CLI tools (alias, dirs, dotfiles, env, files, man-pages, procs, ssh-hosts, text, todo-comments)
    - Just recipes, AWS profiles, fish-history
    
    Platform-conditional channels:
    - Linux: flatpak, journal, systemd-units
    - Darwin: brew-packages

--- modules/home/developer/television.nix
+++ modules/home/developer/television.nix
@@ -1,26 +1,73 @@
-_: {
-  programs = {
-    television = {
-      enable = true;
-      enableFishIntegration = true;
+{ lib, pkgs, ... }:
+let
+  cableDir = "${pkgs.television.src}/cable/unix";
 
-      channels = {
-        fish-history = {
-          metadata = {
-            name = "fish-history";
-          };
+  fromUpstream = name: builtins.fromTOML (builtins.readFile "${cableDir}/${name}.toml");
 
-          source = {
-            command = "fish -c 'history'";
-            no_sort = true;
-            frecency = false;
-          };
-        };
-      };
-    };
+  # Only include channels whose TOML files exist in the current television version.
+  mkChannels =
+    names:
+    lib.genAttrs (builtins.filter (
+      name: builtins.pathExists "${cableDir}/${name}.toml"
+    ) names) fromUpstream;
 
-    # TODO: This would be interesting to replace/extend delta as a pager for git diffs
-    # TODO: Add plasma-manager, etc... to nix-search index
-    # TODO: Combine with comma and nix-locate/nix-index?
+  commonChannels = [
+    "alias"
+    "aws-profiles"
+    "dirs"
+    "docker-compose"
+    "docker-containers"
+    "docker-images"
+    "docker-networks"
+    "docker-volumes"
+    "dotfiles"
+    "env"
+    "files"
+    "fish-history"
+    "gh-issues"
+    "gh-prs"
+    "git-branch"
+    "git-diff"
+    "git-log"
+    "git-reflog"
+    "git-remotes"
+    "git-repos"
+    "git-stash"
+    "git-submodules"
+    "git-tags"
+    "just-recipes"
+    "man-pages"
+    "procs"
+    "ssh-hosts"
+    "text"
+  ];
+
+  linuxChannels = [
+    "flatpak"
+    "journal"
+    "systemd-units"
+  ];
+
+  darwinChannels = [
+    "brew-packages"
+  ];
+
+  platformChannels =
+    lib.optionalAttrs pkgs.stdenv.isLinux (mkChannels linuxChannels)
+    // lib.optionalAttrs pkgs.stdenv.isDarwin (mkChannels darwinChannels);
+in
+{
+  # Channel dependencies not provided by other modules.
+  home.packages = with pkgs; [
+    fd
+  ];
+
+  programs.television = {
+    enable = true;
+    enableFishIntegration = true;
+    settings.default_channel = "fish-history";
+    channels = mkChannels commonChannels // platformChannels;
   };
+
+  # TODO: This would be interesting to replace/extend delta as a pager for git diffs
 }
